292b1bc022290814c7dd2813e076f18c461f14c7
Electronica Care <elshimaafoli@gmail.com>
2022-11-02 15:34:27 +0200

    Signed-off-by: Electronica Care <elshimaafoli@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
 #include <SFML/Graphics.hpp>
-
+using namespace sf;
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(500, 800), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    RenderWindow window(sf::VideoMode(500, 800), "SFML works!");
+    Texture target;
+    Texture knifeTe;
+    Font font;
+    RectangleShape knifeSp(Vector2f(100,100));
+    float angle=0.1;
+    font.loadFromFile("font/arial.ttf");
+    target.loadFromFile("images/target.png");
+    knifeTe.loadFromFile("images/knife-.png");
+    knifeSp.setOrigin(knifeSp.getGlobalBounds().width/2,knifeSp.getGlobalBounds().height/2);
+    knifeSp.setPosition(window.getSize().x/2,700);
+    CircleShape shape(100.f);
+    knifeSp.setTexture(&knifeTe);
+    shape.setTexture(&target);
+    Text score;
+    score.setString("Score");
+    score.setFont(font);
 
     while (window.isOpen())
     {
-        sf::Event event;
+
+        Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
@@ -16,9 +31,12 @@ int main()
         }
 
         window.clear();
-        shape.setOrigin(shape.getGlobalBounds().width/2,shape.getGlobalBounds().height/2);
+        shape.setOrigin(100,100);
         shape.setPosition(window.getSize().x/2,window.getSize().y/3);
+        shape.rotate(angle);
         window.draw(shape);
+        window.draw(score);
+        window.draw(knifeSp);
         window.display();
     }
 
