c364ac6893fc264dd0ba22c828523e45bf373e5a
Electronica Care <elshimaafoli@gmail.com>
2022-11-02 14:33:48 +0200

    Signed-off-by: Electronica Care <elshimaafoli@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(500, 800), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
@@ -16,6 +16,8 @@ int main()
         }
 
         window.clear();
+        shape.setOrigin(shape.getGlobalBounds().width/2,shape.getGlobalBounds().height/2);
+        shape.setPosition(window.getSize().x/2,window.getSize().y/3);
         window.draw(shape);
         window.display();
     }
